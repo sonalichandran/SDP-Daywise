6177c42aa21c284531b055ce34b8445fc974feb5
sonalichandran <124024790+sonalichandran@users.noreply.github.com>
2024-08-12 15:09:36 +0530

    Add files via upload

---
+++ Day 1/Software_Design_Pattern-a60ce9cefa74bc4be3c9a42856d5008b4c69bce8/src/pages/Web/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from '/src/components/Web/ImageSlider'
+
+const Home = () => {
+  return (
+    <>
+    <div className='h-screen w-screen item-center justify-center flex'>
+
+    <Image/>
+    </div>
+    </>
+  )
+}
+
+export default Home
